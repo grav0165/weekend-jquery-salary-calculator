3479841600723dd6f3b73abc8df38b05dd957cb4
Aubrey Graves <grav0165@gmail.com>
2023-06-09 16:22:34 -0500

    created framework with some pseudo code, beginning input boxes

---
+++ salary.js
@@ -0,0 +1,14 @@
+$(document).ready(onReady);
+
+let totalSalaries
+function onReady(){
+
+}
+
+// Create function to take in 5 pieces of information in 5 different text boxes
+    // format the information (such as salary having $ in front)
+
+// on click of submit the 5 pieces of information will be added to the table
+
+// After storing 5 pieces of informatin to the DOM, 
+// add 'annual salary' column up to monthly total amount at the bottom
